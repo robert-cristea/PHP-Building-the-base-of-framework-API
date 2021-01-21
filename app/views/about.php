14a943ce93307ef6395482e23cc5e6c529bea9be
adventure-star <Boristolstyakov@protonmail.com>
2021-01-22 00:46:49 +0700

    total-count to get

---
+++ app/views/about.php
@@ -0,0 +1,21 @@
+<?php
+require APPROOT . '/views/includes/header.php';
+require APPROOT . '/views/includes/navigation.php';
+?>
+
+<main>
+    <div class="row">
+        <div class="container">
+            <div class="col s12 m10">
+                <div class="card-panel ">
+                    <h4>This is blank page</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+
+<?php
+require APPROOT . '/views/includes/footer.php';
+?>
